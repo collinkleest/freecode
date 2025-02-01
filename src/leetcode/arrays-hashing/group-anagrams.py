f4373b51628773c509919130be722cbac73d866a
Collin Kleest <collinkleest@gmail.com>
2025-01-31 22:47:18 -0500

    feat: group ana initial solution

--- src/leetcode/arrays-hashing/group-anagrams.py
+++ src/leetcode/arrays-hashing/group-anagrams.py
@@ -2,7 +2,22 @@
 
 from typing import List
 
-
+"""
+first attempt brute force solution
+time complexity O(n log(n))
+space complexity O(n)
+"""
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        grouped = []
+        anaMap = {}
+        for s in strs:
+            ss = sorted(s)
+            ss = "".join(ss)
+            if ss in anaMap:
+                anaMap[ss].append(s)
+            else:
+                anaMap[ss] = [s]
+        for key in anaMap:
+            grouped.append(anaMap[key])
+        return grouped
