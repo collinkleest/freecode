535b1668de0b6fa77bfd89260d0d0759a3f2c344
Collin Kleest <collinkleest@gmail.com>
2025-03-09 14:15:02 -0400

    feat(stack): min stack improved

--- src/leetcode/stack/min-stack.py
+++ src/leetcode/stack/min-stack.py
@@ -27,3 +27,37 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.sortedStack[0]
+
+
+"""
+keep track of min value when adding to stack
+keep tuples in stack 
+each stack element maps to (val, minVal)
+faster time complexity O(1)
+space complexity O(n)
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if self.stack:
+            topMin = self.getMin()
+            if topMin < val:
+                self.stack.append(tuple([val, topMin]))
+            else:
+                self.stack.append(tuple([val, val]))
+        else:
+            self.stack.append(tuple([val, val]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[len(self.stack) - 1][0]
+
+    def getMin(self) -> int:
+        return self.stack[len(self.stack) - 1][1]
