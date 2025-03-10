71a188c750428d5d4abbac8d4dfc8eb1170bac74
Collin Kleest <collinkleest@gmail.com>
2025-03-09 22:41:31 -0400

    feat(hashmap): ransom note

---
+++ src/leetcode/hashmap/ransom-note.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/ransom-note
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNoteMap, magazineMap = dict(), dict()
+
+        for char in ransomNote:
+            ransomNoteMap[char] = ransomNoteMap.get(char, 0) + 1
+
+        for char in magazine:
+            magazineMap[char] = magazineMap.get(char, 0) + 1
+
+        for char, count in ransomNoteMap.items():
+            if char not in magazineMap:
+                return False
+            elif magazineMap[char] < count:
+                return False
+        return True
