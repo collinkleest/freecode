c9ac750a5431f825919842d769cf85e0847e0031
Collin Kleest <collinkleest@gmail.com>
2025-03-10 20:19:48 -0400

    feat(linked-list): merge sorted list

--- src/leetcode/linked-list/merge-sorted-lists.py
+++ src/leetcode/linked-list/merge-sorted-lists.py
@@ -46,3 +46,32 @@ class Solution:
             result.next = temp
             result = temp
         return head
+
+
+"""
+much better
+time complexity O(N)
+space complexity O(1)
+"""
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        temp = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            temp = temp.next
+
+        if list1:
+            temp.next = list1
+        else:
+            temp.next = list2
+
+        return dummy.next
